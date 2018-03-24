df102094fada064fc5f9d203c4cb6faa06a8c4c6
SUSHMITA YADAV <sushmita.mca17.du@gmail.com>
2018-03-24 22:32:46 +0530

    Implements Stack using STL Queues

---
+++ stack_using_queues.cpp
@@ -0,0 +1,131 @@
+//Program to implement a Stack using two Queues
+
+#include<iostream>
+#include<cstdlib>
+#include<queue>
+
+using namespace std;
+
+class stack{
+
+	queue<int> q1,q2;
+	int curr_size;
+	public:
+	   void push(int);
+	   void pop();
+           int top();
+	   bool isEmpty();
+	   void stack_status();
+};
+
+void stack::push(int ele){
+    /*
+    Objective: To push an element onto the stack.
+    Input: ele -> The element to be pushed
+    Output: None
+    Return value: None
+    */
+   curr_size++;
+   q2.push(ele);
+   while(!q1.empty()){
+		q2.push(q1.front());
+		q1.pop();
+   }
+   queue<int> temp = q1;
+   q1 = q2;
+   q2 = temp;
+}
+
+bool stack::isEmpty(){
+    if(q1.empty())
+	return true;	
+}
+
+void stack::pop(){
+    /*
+    Objective: To pop the top element out of the stack.
+    Input: None
+    Output: None
+    Return value: top element, if stack is not empty
+                  NULL, if stack is empty
+    */
+    if(!isEmpty()){
+	    curr_size--;
+	    q1.pop();
+    }
+}
+
+int stack::top(){
+    /*
+    Objective: To find the top element of the stack.
+    Input: None
+    Output: None
+    Return value: top element, if stack is not empty
+                  NULL, if stack is empty
+    */
+    if( !isEmpty() )
+	return q1.front();
+}
+
+void stack::stack_status(){
+    /*
+    Objective: To print the stack
+    Input: None
+    Output: Stack data
+    Return value: None
+    */
+    cout<<"\n\n------Stack------\n";
+    if(!isEmpty()){
+	queue<int> temp = q1;
+	while(!temp.empty()){
+		cout<<"\t"<<temp.front()<<"\n";
+		temp.pop();		
+	}
+    }
+    cout<<"-----------------";
+}
+
+int main(){
+    /*
+    Objective: To perform various stack operations.
+    */
+    stack s;
+    int choice;
+    while(1){
+    cout<<"\n\n\t\t\tSTACK OPERATIONS\n\n1.PUSH\n2.POP\n3.TOP\n4.PRINT STACK\n5.EXIT\n\n";
+    cout<<"\nEnter your choice : ";
+    cin>>choice;
+    switch(choice){
+        case 1: cout<<"\nEnter element: ";
+		int ele;
+		cin>>ele;
+                s.push(ele);
+                s.stack_status();
+            break;
+            
+        case 2: if(!s.isEmpty()){
+			s.pop();
+                	cout<<"\nElement popped!!";
+                }
+            else
+                cout<<"\nStack empty!!Cannot pop!!";
+	    s.stack_status();
+            break;
+                
+        case 3: if(!s.isEmpty())
+                cout<<"\nTop element: "<<s.top();
+            else
+                cout<<"\nStack is empty!!";
+            break;
+                
+        case 4: s.stack_status();
+            break;
+                
+        case 5: exit(0);
+        
+        default: cout<<"Invalid choice!!";
+            break;
+    }
+    }
+return 0;
+}
